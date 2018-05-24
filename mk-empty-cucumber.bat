39116ed32f3939696a7d1aeb6052ea25ee8eb336
yahmedov <ahmedov.yuksel@gmail.com>
2018-05-24 14:50:02 -0700

    upd cucumber-archetype version 2.3.1.2
    script creates an empty cucumber project

--- mk-empty-cucumber.bat
+++ mk-empty-cucumber.bat
@@ -1,4 +1,4 @@
 REM You need to run the bat via cmd and pass it a name as an argument
 REM Example: mk-empty-cucumber.bat ProjectName
 set name=%1
-mvn archetype:generate -B -DgroupId=%name% -DartifactId=%name% -Dpackage=%name% -DarchetypeVersion=1.3 > ./console.log
+mvn archetype:generate -B -DgroupId=%name% -DartifactId=%name% -Dpackage=%name% -DarchetypeGroupId=io.cucumber -DarchetypeArtifactId=cucumber-archetype -DarchetypeVersion=2.3.1.2 > ./console.log
