8d9140205c6919bf4c6db1604c1f673eab3d8505
yahmedov <ahmedov.yuksel@gmail.com>
2018-05-25 14:32:32 +0300

    upd script to print console log

--- mk-empty-cucumber.bat
+++ mk-empty-cucumber.bat
@@ -1,16 +1,17 @@
 @echo off
 :: the "@echo off" line sets the whole batch file not to print commands
-:: You need to run the bat via cmd and pass it a name as an argument
+:: You need to run the bat via CMD and pass it a name as an argument
 :: Example: mk-empty-cucumber.bat ProjectName
 set projectName=%1
 :: the caret sign ^ is used to split a long command into multiple lines
 mvn archetype:generate ^
--B ^
 -DgroupId=%projectName% ^
 -DartifactId=%projectName% ^
 -Dpackage=%projectName% ^
 -DarchetypeGroupId=io.cucumber ^
 -DarchetypeArtifactId=cucumber-archetype ^
--DarchetypeVersion=2.3.1.2 > .\console.log | echo Creating an empty cucumber project ...
+-DarchetypeVersion=2.3.1.2 ^
+-DinteractiveMode=false > .\console.log | echo Creating an empty cucumber project ...
 echo Done!
-echo Project is created in "%cd%\%projectName%"
+::echo Project is created in "%cd%\%projectName%"
+type .\console.log
