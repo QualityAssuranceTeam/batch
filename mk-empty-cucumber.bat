39cb40a8a665ea4449b97cb16861454fa7d136a2
yahmedov <ahmedov.yuksel@gmail.com>
2018-05-25 13:03:50 +0300

    upd script

--- mk-empty-cucumber.bat
+++ mk-empty-cucumber.bat
@@ -1,4 +1,16 @@
-REM You need to run the bat via cmd and pass it a name as an argument
-REM Example: mk-empty-cucumber.bat ProjectName
-set name=%1
-mvn archetype:generate -B -DgroupId=%name% -DartifactId=%name% -Dpackage=%name% -DarchetypeGroupId=io.cucumber -DarchetypeArtifactId=cucumber-archetype -DarchetypeVersion=2.3.1.2 > ./console.log
+@echo off
+:: the "@echo off" line sets the whole batch file not to print commands
+:: You need to run the bat via cmd and pass it a name as an argument
+:: Example: mk-empty-cucumber.bat ProjectName
+set projectName=%1
+:: the caret sign ^ is used to split a long command into multiple lines
+mvn archetype:generate ^
+-B ^
+-DgroupId=%projectName% ^
+-DartifactId=%projectName% ^
+-Dpackage=%projectName% ^
+-DarchetypeGroupId=io.cucumber ^
+-DarchetypeArtifactId=cucumber-archetype ^
+-DarchetypeVersion=2.3.1.2 > .\console.log | echo Creating an empty cucumber project ...
+echo Done!
+echo Project is created in "%cd%\%projectName%"
